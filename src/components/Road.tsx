ead0ff219c51f0f897ea3c49a4f10eca227abbe2
Gabriel Chartier <gabriel@chartier.link>
2022-03-16 16:22:55 -0400

    Add various images, gifs, and svgs

---
+++ src/components/Road.tsx
@@ -0,0 +1,28 @@
+export function Road() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 580 294"
+      height={1000}
+      width={2000}
+      version="1.0"
+      style={{ overflow: 'hidden' }}
+    >
+      <path
+        fill="#000"
+        d="M300.6 314.64h132l224 294h-580l224-294z"
+        transform="translate(-76.603 -314.64)"
+      />
+      <path
+        fill="#fff"
+        d="M306.55 314.64h-3.59l-218.36 294h16.61l205.34-294zM430.22 314.64h-3.56l205.34 294h16.6l-218.38-294z"
+        transform="translate(-76.603 -314.64)"
+      />
+      <path
+        fill="#fff"
+        d="M783.44 530.44h-9.66l-1.62 80.87h12.53l-1.25-80.87zM775.84 429.34l-1.53 76.22h8.75l-1.15-76.22h-6.07zM780.94 365.47h-3.82l-.93 45.72h5.43l-.68-45.72zM780.38 329.47h-2.54l-.5 24.53h3.41l-.37-24.53zM778.09 317.31l-.12 6.44h2.31l-.09-6.44h-2.1z"
+        transform="translate(-488.423 -317.316)"
+      />
+    </svg>
+  );
+}
